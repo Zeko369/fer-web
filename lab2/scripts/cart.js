af10e1b6b1cca7d038f5cf7b3be465a2167328e6
Fran Zekan <zekan.fran369@gmail.com>
2021-04-16 13:45:15 +0200

    lab[2]: get my task working

--- lab2/scripts/cart.js
+++ lab2/scripts/cart.js
@@ -1,14 +1,22 @@
+// @ts-check
+/// <reference path="./index.d.ts" />
+
 const productURL = (id) => `https://web1lab2.azurewebsites.net/products/${id}`;
 const cartBody = document.querySelector("#cart-body-items");
+
+window.cache = [];
 
 const loadCart = async () => {
   cartBody.innerHTML = "<p>Loading...</p>";
 
   const items = [];
+  window.cache = [];
+  // @ts-ignore, what?
   for (const [id, quantity] of Object.entries(window.cartService.items)) {
     const res = await fetch(productURL(id));
     const data = await res.json();
 
+    window.cache.push(data);
     items.push({ ...data, quantity });
   }
 
@@ -16,12 +24,12 @@ const loadCart = async () => {
 };
 
 const renderCart = async (items) => {
+  /** @type {HTMLElement} */
   const rowTemplate = document.querySelector("#cart-template-item");
   cartBody.innerHTML = "";
 
-  console.log(rowTemplate);
-
   for (const item of items) {
+    // @ts-ignore
     const template = rowTemplate.content.cloneNode(true);
     template.querySelector(".cart-item-title").textContent = item.name;
     template.querySelector(".cart-item-price").textContent = `${item.price} kn`;
@@ -29,4 +37,28 @@ const renderCart = async (items) => {
 
     cartBody.appendChild(template);
   }
+};
+
+/**
+ * @param {string} key
+ * @returns
+ */
+const compareFunction = (key) => {
+  return (a, b) => (a[key] < b[key] ? -1 : 1);
+};
+
+/** * @param {string} key */
+const sortBy = (key) => {
+  const data = window.cache.map((item) => ({
+    ...item,
+    quantity: window.cartService.items[item.id]
+  }));
+
+  if (!["name", "price", "quantity"].includes(key)) {
+    console.error("Wrong key");
+    return;
+  }
+
+  const sorted = data.sort(compareFunction(key));
+  renderCart(sorted);
 };
--- lab2/scripts/index.d.ts
+++ lab2/scripts/index.d.ts
@@ -1,5 +1,12 @@
 export declare global {
   interface Window {
     cartService: CartService;
+    cache: {
+      id: number;
+      name: string;
+      price: number;
+      categoryId: number;
+      imageUrl: string;
+    }[];
   }
 }
